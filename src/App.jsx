82437eba72f38c1be75ee3b37e33514d81626c1e
13swati <swatigupta11330@gmail.com>
2024-06-23 00:00:36 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Swati Gupta</h1>
+        <p>IT Developer</p>
+      </header>
+      <section>
+        <h2>About Me</h2>
+        <p>Enthusiastic beginner programmer with good fundation in C and C++ and a passion for web development.
+        </p>
+      </section>
+      <section>
+        <h2>Professional:</h2>
+        <ul>
+          <li>
+            <h3></h3>
+            <p>Time Management</p>
+            <p>
+              Communication
+            </p>
+          </li>
+          <li>
+            <h3></h3>
+            <p></p>
+            <p>
+              
+            </p>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2>Education</h2>
+        <ul>
+          <li>
+            <h3>Government College of Engineering,Karad.</h3>
+            <p>Bachelor of Technology in Information Technology.</p>
+            <p>Graduation:2027(Expected)</p>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2>Skills</h2>
+        <ul>
+          <li>C Programming Language</li>
+          <li>C++ Programming Language</li>
+          <li>Data structure</li>
+          
+        </ul>
+      </section>
+      <footer>
+        <p>Contact: swatigupta113302gmail.com</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
+
